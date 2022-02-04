77c7d9ab1ce830b3a49f5833a28be679d892c86f
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-05 00:24:03 +0530

    UC3 Handle Empty Null Exception of Mood Analyser

---
+++ MoodAnalyse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyse
+    {
+        private string message;
+        public MoodAnalyse()
+        {
+
+        }
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMethod()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.TypeOfException.EMPTY_MESSAGE, "mood should not be empty");
+                else if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.TypeOfException.NULL_MESSAGE, "Mood Should not be NULL");
+            }
+        }
+    }
+}
---
+++ MoodAnalyserException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserCustomException : ApplicationException
+    {
+        public enum TypeOfException
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE, NO_SUCH_FIELD, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+        }
+        public TypeOfException type;
+        public MoodAnalyserCustomException(TypeOfException type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
